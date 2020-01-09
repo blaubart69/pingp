de0898cea124766aaa65397d44b5eb598a0b05a8
Bernhard Spindler <bernhard.spindler@unicredit.at>
2020-01-09 09:12:47 +0100

    removed unused usings

--- Opts.cs
+++ Opts.cs
@@ -1,10 +1,6 @@
-﻿using System;
+﻿using Spi;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Spi;
 
 namespace pingp
 {
--- Program.cs
+++ Program.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Spi;
+using System;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
-using System.Net.Sockets;
-using System.Text;
 using System.Threading.Tasks;
-
-using Spi;
 
 namespace pingp
 {
